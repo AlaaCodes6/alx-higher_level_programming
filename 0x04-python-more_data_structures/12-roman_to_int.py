91d0f0e747a644758cee16a9c4d6cfb1430b141b
AlaaCodes6 <alaacodes6@gmail.com>
2023-12-06 14:00:10 -0800

    12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,11 +1,40 @@
 #!/usr/bin/python3
+def subtract(list_num):
+    to_subtract = 0
+    max_list = max(list_num)
+
+    for num in list_num:
+        if max_list > num:
+            to_subtract += num
+
+    return (max_list - to_subtract)
+
+
 def roman_to_int(roman_string):
+    if not roman_string:
+        return 0
+
     if not isinstance(roman_string, str):
         return 0
-    total = 0
+
+    r_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    list_keys = list(r_dict.keys())
+
     n = 0
-    roman_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    for i in reversed(roman_string):
-        n = roman_n[i]
-        total += n if total < n * 5 else -n
-        return total
+    last_rom = 0
+    list_num = [0]
+
+    for i in roman_string:
+        for r_num in list_keys:
+            if r_num == i:
+                if r_dict.get(i) <= last_rom:
+                    num += subtract(list_num)
+                    list_num = [r_dict.get(i)]
+                else:
+                    list_num.append(r_dict.get(i))
+
+                last_rom = r_dict.get(i)
+
+    n += subtract(list_num)
+
+    return (n)
--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
     list2 = list(a_dictionary.keys())
-    list2.sort() 
+    list2.sort()
     for i in list2:
         print("{}: {}".format(i, a_dictionary.get(i)))

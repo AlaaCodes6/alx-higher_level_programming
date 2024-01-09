cc7e9985e447cf98da71e1e06f1b5105b7a908a3
AlaaCodes6 <alaacodes6@gmail.com>
2024-01-09 03:55:40 +0200

    'tasks'

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""subclass Square."""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """square subclass"""
+
+    def __init__(self, size):
+        """new square.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """
+        A function that finds the area of the Square
+        """
+        return self.__size ** 2
---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""writes a base geometry class BaseGeometry."""
+
+
+class BaseGeometry:
+    """base geometry."""
+
+    def area(self):
+        """Raises an error"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validate a value.
+
+        Args:
+            name (str): The name of the parameter.
+            value (int): The parameter to validate.
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is <= 0.
+        """
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
